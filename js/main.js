743c8bdfd5ec152323301daf623bb9c1e8facff0
Agata Rojewska <agata.rojewska@gmail.com>
2024-05-14 18:02:00 +0200

    Add icons to contact

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+const nav = document.querySelector(".navbar-collapse")
 const footerYear = document.querySelector(".footer-year")
+
+document.addEventListener("click", () => {
+  if (nav.classList.contains("show")) {
+    nav.classList.remove("show")
+  }
+})
 
 const handleCurrentYear = () => {
   const year = new Date().getFullYear()
